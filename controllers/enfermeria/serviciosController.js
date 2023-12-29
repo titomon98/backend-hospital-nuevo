bad4907b84c0fd1ab9ea3060112d87ce53e86d00
titomon98 <arturomonterroso3@gmail.com>
2023-12-29 02:45:04 -0600

    Agregando migraciones finales para sistema de manera temporal

---
+++ controllers/enfermeria/serviciosController.js
@@ -0,0 +1,150 @@
+'use strict'
+const Sequelize     = require('sequelize');
+const db = require("../../models");
+const Contrato = db.contratos;
+const Op = db.Sequelize.Op;
+
+module.exports = {
+    create(req, res) {
+        let form = req.body.form
+        const datos = {
+            contrato: form.contrato,
+            nombre: form.nombre,
+            estado: 1
+        };
+
+        Contrato.create(datos)
+        .then(tipo => {
+            res.send(tipo);
+        })
+        .catch(error => {
+            console.log(error)
+            return res.status(400).json({ msg: 'Ha ocurrido un error, por favor intente más tarde' });
+        });
+                    
+    },
+
+ 
+    list(req, res) {
+        const getPagingData = (data, page, limit) => {
+            const { count: totalItems, rows: referido } = data;
+
+            const currentPage = page ? +page : 0;
+            const totalPages = Math.ceil(totalItems / limit);
+
+            return { totalItems, referido, totalPages, currentPage };
+        };
+
+
+        const getPagination = (page, size) => {
+            const limit = size ? +size : 2;
+            const offset = page ? page * limit : 0;
+
+            return { limit, offset };
+        };
+
+        const busqueda=req.query.search;
+        const page=req.query.page-1;
+        const size=req.query.limit;
+        const criterio=req.query.criterio;
+        const order=req.query.order;
+
+
+        const { limit, offset } = getPagination(page, size);
+
+        var condition = busqueda ? { [Op.or]: [{ contrato: { [Op.like]: `%${busqueda}%` } }] } : null ;
+
+        Contrato.findAndCountAll({ where: condition,order:[[`${criterio}`,`${order}`]],limit,offset})
+        .then(data => {
+
+        console.log('data: '+JSON.stringify(data))
+        const response = getPagingData(data, page, limit);
+
+        console.log('response: '+JSON.stringify(response))
+        res.send({total:response.totalItems,last_page:response.totalPages, current_page: page+1, from:response.currentPage,to:response.totalPages,data:response.referido});
+        })
+        .catch(error => {
+            console.log(error)
+            return res.status(400).json({ msg: 'Ha ocurrido un error, por favor intente más tarde' });
+        });
+    },
+
+
+    find (req, res) {
+        const id = req.params.id;
+
+        return Contrato.findByPk(id)
+        .then(marca => res.status(200).send(marca))
+        .catch(error => res.status(400).send(error))
+    },
+
+    update (req, res) {
+        let form = req.body.form
+        Contrato.update(
+            { 
+                contrato: form.contrato,
+                nombre: form.nombre,
+            },
+            { where: { 
+                id: form.id 
+            } }
+        )
+        .then(marca => res.status(200).send('El registro ha sido actualizado'))
+        .catch(error => {
+            console.log(error)
+            return res.status(400).json({ msg: 'Ha ocurrido un error, por favor intente más tarde' });
+        });
+    },
+
+    activate (req, res) {
+        Contrato.update(
+            { estado: 1 },
+            { where: { 
+                id: req.body.id 
+            } }
+        )
+        .then(marca => res.status(200).send('El registro ha sido activado'))
+        .catch(error => {
+            console.log(error)
+            return res.status(400).json({ msg: 'Ha ocurrido un error, por favor intente más tarde' });
+        });
+    },
+
+    deactivate (req, res) {
+        Contrato.update(
+            { estado: 0 },
+            { where: { 
+                id: req.body.id 
+            } }
+        )
+        .then(marca =>res.status(200).send('El registro ha sido desactivado'))
+        .catch(error => {
+            console.log(error)
+            return res.status(400).json({ msg: 'Ha ocurrido un error, por favor intente más tarde' });
+        });
+    },
+    get (req, res) {
+        Contrato.findAll({attributes: ['id', 'contrato']})
+        .then(data => {
+            res.send(data);
+        })
+        .catch(error => {
+            console.log(error)
+            return res.status(400).json({ msg: 'Ha ocurrido un error, por favor intente más tarde' });
+        });
+    },
+    getSearch (req, res) {
+        var busqueda = req.query.search;
+        var condition = busqueda?{ [Op.or]:[ {contrato: { [Op.like]: `%${busqueda}%` }}],[Op.and]:[{estado:1}] } : {estado:1} ;
+        Contrato.findAll({
+            where: condition})
+        .then(data => {
+            res.send(data);
+        })
+        .catch(error => {
+            console.log(error)
+            return res.status(400).json({ msg: 'Ha ocurrido un error, por favor intente más tarde' });
+        });
+    }
+};
+
