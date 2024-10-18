d93ca7a4b16c956bc46820e544d78fd5bdc1612b
titomon98 <arturomonterroso3@gmail.com>
2024-10-17 18:50:37 -0600

    Agregar cambios a cuentas controller

--- controllers/caja/cuentasController.js
+++ controllers/caja/cuentasController.js
@@ -124,7 +124,7 @@ module.exports = {
             where: condition,order:[[`${criterio}`,`${order}`]],limit,offset})
         .then(data => {
 
-        console.log('data: '+JSON.stringify(data.rows[0].expedientes))
+        console.log('data: '+JSON.stringify(data.rows[0].Expediente))
         const response = getPagingData(data, page, limit);
 
         console.log('response: '+JSON.stringify(response))
