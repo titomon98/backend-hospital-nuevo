afc5fe83ff7584d7296eeab6b3bec2ad84b52a94
Arturo Monterroso <45072156+titomon98@users.noreply.github.com>
2025-05-14 16:06:16 -0600

    Update cuentasController.js

--- controllers/caja/cuentasController.js
+++ controllers/caja/cuentasController.js
@@ -114,7 +114,7 @@ module.exports = {
         const order=req.query.order;
         const { limit, offset } = getPagination(page, size);
         //var condition = {estado:1, '$Expediente.solvencia$': 0, [Op.or]:[{'$Expediente.estado$': 0},{'$Expediente.estado$': 6},{'$Expediente.estado$': 7},{'$Expediente.estado$': 8},{'$Expediente.estado$': 9}]}
-        var condition = {estado:1, '$Expediente.solvencia$': 0}
+        var condition = {estado:1}
         /* var condition = {
             [Op.and]: [
                 { estado: 1 },
